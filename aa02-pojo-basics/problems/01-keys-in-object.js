de02ae003768c3890e816a48e70f5781735fc672
Your Name <Your Email>
2024-05-09 20:42:33 -0400

    pr1 finished

--- aa02-pojo-basics/problems/01-keys-in-object.js
+++ aa02-pojo-basics/problems/01-keys-in-object.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a function called `keysInObject(obj)` that takes in an object and returns 
+Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
@@ -12,16 +12,15 @@ keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
-// function keysInObject(obj) {
-  // Your code here 
+function keysInObject(obj) {
+  // Your code here
+      let keys = [];
+    for (let key in obj) {
+      keys.push(key);
+    }
+    return keys;
+  };
 
-//     let keys = [];
-//     for (let key in obj) {
-//       keys.push(key);
-//     }
-//     return keys;
-//   };
-// }
 
 // let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
 // let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
@@ -29,12 +28,12 @@ keysInObject(foods); // => ["apple", "lemon", "mango"]
 // console.log(keysInObject(foods)); // => ["apple", "lemon", "mango"]
 
 
-const keysInObject = (obj) => Object.keys(obj);
+// const keysInObject = (obj) => Object.keys(obj);
 
-let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
-let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
-console.log(keysInObject(animals)); // => ["dog", "cat", "bison"]
-console.log(keysInObject(foods)); // => ["apple", "lemon", "mango"]
+// let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+// let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+// console.log(keysInObject(animals)); // => ["dog", "cat", "bison"]
+// console.log(keysInObject(foods)); // => ["apple", "lemon", "mango"]
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
